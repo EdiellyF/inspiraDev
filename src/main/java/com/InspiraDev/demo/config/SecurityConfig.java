044d10fd2999231d44510f4d0449ae087710cba6
Ediellyf <ediellyferreiradecarvalho@gmail.com>
2025-03-19 10:11:09 -0300

    Removendo arquivos sensíveis do repositório

--- src/main/java/com/InspiraDev/demo/config/SecurityConfig.java
+++ src/main/java/com/InspiraDev/demo/config/SecurityConfig.java
@@ -34,4 +34,6 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return  http.build();
     }
+
+
 }
